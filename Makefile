6542cc918fe0f2e954f40ceb5ae805cebd4ebfc2
Rahij Ramsharan <rahij.ramsharan@gmail.com>
2023-12-09 12:52:30 +0000

    single node - dirty

--- Makefile
+++ Makefile
@@ -6,3 +6,6 @@ echo: build
 
 unique: build
 	./maelstrom/maelstrom test -w unique-ids --bin ./target/debug/echo --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
+
+broadcast: build
+	./maelstrom/maelstrom test -w broadcast --bin ./target/debug/echo --node-count 1 --time-limit 20 --rate 10
--- echo/src/main.rs
+++ echo/src/main.rs
@@ -6,7 +6,8 @@ use async_trait::async_trait;
 use maelstrom::protocol::{Message, MessageBody};
 use maelstrom::{done, Node, Result, Runtime};
 use serde_json::{json, Map};
-use std::sync::Arc;
+use std::collections::HashSet;
+use std::sync::{Arc, Mutex};
 use uuid::Uuid;
 
 pub(crate) fn main() -> Result<()> {
@@ -14,25 +15,82 @@ pub(crate) fn main() -> Result<()> {
 }
 
 async fn try_main() -> Result<()> {
-    let handler = Arc::new(Handler::default());
+    let handler = Arc::new(Handler::new());
     Runtime::new().with_handler(handler).run().await
 }
 
 #[derive(Clone, Default)]
-struct Handler {}
+struct Handler {
+    message_ids: Arc<Mutex<HashSet<u64>>>,
+}
+
+impl Handler {
+    fn new() -> Handler {
+        return Handler {
+            message_ids: Arc::new(Mutex::new(HashSet::new())),
+        };
+    }
+
+    fn store_message(&self, message_id: u64) {
+        Arc::clone(&self.message_ids)
+            .lock()
+            .unwrap()
+            .insert(message_id);
+    }
+
+    fn retreieve_messages(&self) -> Result<Vec<u64>> {
+        Ok(Arc::clone(&self.message_ids)
+            .lock()
+            .unwrap()
+            .clone()
+            .into_iter()
+            .collect())
+    }
+}
 
 #[async_trait]
 impl Node for Handler {
     async fn process(&self, runtime: Runtime, req: Message) -> Result<()> {
         if req.get_type() == "echo" {
-            let echo = req.body.clone().with_type("echo_ok");
-            return runtime.reply(req, echo).await;
+            let reply = req.body.clone().with_type("echo_ok");
+            return runtime.reply(req, reply).await;
         }
         if req.get_type() == "generate" {
             let reply = req.body.clone().with_type("generate_ok");
             let mut extra = Map::new();
             extra.insert("id".to_string(), json!(Uuid::new_v4().to_string()));
             return runtime.reply(req, MessageBody { extra, ..reply }).await;
+        }
+        if req.get_type() == "broadcast" {
+            let reply = req.body.clone().with_type("broadcast_ok");
+            self.store_message(reply.extra.get("message").unwrap().as_u64().unwrap());
+            return runtime
+                .reply(
+                    req,
+                    MessageBody {
+                        extra: Map::new(),
+                        ..reply
+                    },
+                )
+                .await;
+        }
+        if req.get_type() == "read" {
+            let reply = req.body.clone().with_type("read_ok");
+            let mut extra = Map::new();
+            extra.insert("messages".to_string(), self.retreieve_messages()?.into());
+            return runtime.reply(req, MessageBody { extra, ..reply }).await;
+        }
+        if req.get_type() == "topology" {
+            let reply = req.body.clone().with_type("topology_ok");
+            return runtime
+                .reply(
+                    req,
+                    MessageBody {
+                        extra: Map::new(),
+                        ..reply
+                    },
+                )
+                .await;
         }
 
         done(runtime, req)
